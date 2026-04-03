96d756743b6a617dbad77463bbf90a3e0580bf36
Neo Mashigo <neo.mashigo927@gmail.com>
2026-04-04 01:51:04 +0200

    Add header to ProfilePage

--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback, useMemo, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './ProfilePage.css';
 import { 
